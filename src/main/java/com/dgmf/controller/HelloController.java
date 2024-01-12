83e1c8136d18b6a60875474f195d53d89ec2cd68
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-12 16:41:10 +0100

    Enhancing Spring MVC Controller to provide HTML response

--- src/main/java/com/dgmf/controller/HelloController.java
+++ src/main/java/com/dgmf/controller/HelloController.java
@@ -12,4 +12,24 @@ public class HelloController {
     public String sayHello() {
         return "Hello, what are you learning today ?";
     }
+
+    // http://localhost:8080/hello-html
+    @RequestMapping("/hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<!DOCTYPE html>");
+        sb.append("<html lang=\"en\">");
+        sb.append("<head>");
+        sb.append("<meta charset=\"UTF-8\">");
+        sb.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+        sb.append("<title>My First HTML Page</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My First HTML Page with Body.");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
+    }
 }
