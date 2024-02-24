b1c9dd7428f695867f743b8ba564eb49570d179b
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-24 06:39:30 +0100

    Creating a Login Form

--- src/main/java/com/dgmf/controller/login/LoginController.java
+++ src/main/java/com/dgmf/controller/login/LoginController.java
@@ -1,29 +1,20 @@
 package com.dgmf.controller.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
     // http://localhost:8080/login
     // http://localhost:8080/login?name=Glaz
+//    @RequestMapping("/login")
+//    public String login(@RequestParam String name, ModelMap modelMap) {
+//        modelMap.put("name", name);
+//
+//        return "login";
+//    }
     @RequestMapping("/login")
-    public String login(@RequestParam String name, ModelMap modelMap) {
-        modelMap.put("name", name);
-
-        // Logging Statements
-        logger.debug("Request Param is : {}", name);
-        logger.info("Request Param is : {}", name);
-        logger.warn("Request Param is : {}", name);
-
-        // Not Recommended for Prod Code
-        System.out.println("Request Param is : " + name);
+    public String login() {
 
         return "login";
     }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,13 @@
         <title>Login Page</title>
     </head>
     <body>
-        Welcome ${name} to the Login Page.
+        <!-- Welcome ${name} to the Login Page. -->
+        Welcome to the Login Page.
+
+        <form action="" method="post">
+            Name : <input type="text" name="name">
+            Password : <input type="password" name="password">
+            <input type="submit">
+        </form>
     </body>
 </html>
